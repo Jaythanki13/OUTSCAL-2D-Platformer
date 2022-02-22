bef5eb290dfb01c65c5b4c84e597ed33327a1e2f
Jay Thanki <thankijay15@gmail.com>
2022-02-22 22:08:46 +0530

    Feature_2 Player Controller

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //[SerializeField]float speed = 20f;
+    [SerializeField]Animator animator;
+
+
     // void OnCollisionEnter2D(Collision2D collision)
     // {
     //     Debug.Log("Collision:- " + collision.gameObject.name);
@@ -11,6 +15,36 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", horizontal);
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        Vector3 Scale = transform.localScale;
+        if (horizontal < 0)
+        {
+            Scale.x = -1 * Mathf.Abs(Scale.x);
+            Debug.Log("Flipped....");
+        }
+
+        else if (horizontal > 0)
+        {
+            Scale.x = Mathf.Abs(Scale.x);
+        }
+
+        else if (horizontal == 0)
+        {
+            
+        }
+
+        transform.localScale = Scale;
+
         
+        float vertical = Input.GetAxisRaw("Vertical");
+        animator.SetBool("Jumping", true);
+
+
+        //PlayerMovement(horizontal);
     }
+
+
 }
