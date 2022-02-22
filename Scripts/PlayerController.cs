b54d34f4236c65f97ae794db228c10e6b935dc3a
Jay Thanki <thankijay15@gmail.com>
2022-02-22 20:58:50 +0530

    Animation after Feature 2 Implementation

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     Debug.Log("Collision:- " + collision.gameObject.name);
+    // }
+
+    private void Update()
+    {
+        
+    }
+}
